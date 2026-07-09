9586bd3677db350231d63cd552a4e5f99fdbf0f2
BUILDORA <bjosephbrooks07@gmail.com>
2026-07-09 15:13:57 -0400

    Create countries.js

---
+++ js/assets/countries.js
@@ -0,0 +1,777 @@
+// ==========================================
+// NetView
+// Countries
+// Part 1/3
+// ==========================================
+
+export const COUNTRIES = [
+
+{
+    code: "AF",
+    name: "Afghanistan",
+    flag: "🇦🇫"
+},
+
+{
+    code: "ZA",
+    name: "Afrique du Sud",
+    flag: "🇿🇦"
+},
+
+{
+    code: "AL",
+    name: "Albanie",
+    flag: "🇦🇱"
+},
+
+{
+    code: "DZ",
+    name: "Algérie",
+    flag: "🇩🇿"
+},
+
+{
+    code: "DE",
+    name: "Allemagne",
+    flag: "🇩🇪"
+},
+
+{
+    code: "AD",
+    name: "Andorre",
+    flag: "🇦🇩"
+},
+
+{
+    code: "AO",
+    name: "Angola",
+    flag: "🇦🇴"
+},
+
+{
+    code: "SA",
+    name: "Arabie saoudite",
+    flag: "🇸🇦"
+},
+
+{
+    code: "AR",
+    name: "Argentine",
+    flag: "🇦🇷"
+},
+
+{
+    code: "AM",
+    name: "Arménie",
+    flag: "🇦🇲"
+},
+
+{
+    code: "AU",
+    name: "Australie",
+    flag: "🇦🇺"
+},
+
+{
+    code: "AT",
+    name: "Autriche",
+    flag: "🇦🇹"
+},
+
+{
+    code: "BS",
+    name: "Bahamas",
+    flag: "🇧🇸"
+},
+
+{
+    code: "BD",
+    name: "Bangladesh",
+    flag: "🇧🇩"
+},
+
+{
+    code: "BE",
+    name: "Belgique",
+    flag: "🇧🇪"
+},
+
+{
+    code: "BJ",
+    name: "Bénin",
+    flag: "🇧🇯"
+},
+
+{
+    code: "BO",
+    name: "Bolivie",
+    flag: "🇧🇴"
+},
+
+{
+    code: "BA",
+    name: "Bosnie-Herzégovine",
+    flag: "🇧🇦"
+},
+
+{
+    code: "BW",
+    name: "Botswana",
+    flag: "🇧🇼"
+},
+
+{
+    code: "BR",
+    name: "Brésil",
+    flag: "🇧🇷"
+},
+
+{
+    code: "BG",
+    name: "Bulgarie",
+    flag: "🇧🇬"
+},
+
+{
+    code: "BF",
+    name: "Burkina Faso",
+    flag: "🇧🇫"
+},
+
+{
+    code: "BI",
+    name: "Burundi",
+    flag: "🇧🇮"
+},
+
+{
+    code: "KH",
+    name: "Cambodge",
+    flag: "🇰🇭"
+},
+
+{
+    code: "CM",
+    name: "Cameroun",
+    flag: "🇨🇲"
+},
+
+{
+    code: "CA",
+    name: "Canada",
+    flag: "🇨🇦"
+},
+
+{
+    code: "CV",
+    name: "Cap-Vert",
+    flag: "🇨🇻"
+},
+
+{
+    code: "CL",
+    name: "Chili",
+    flag: "🇨🇱"
+},
+
+{
+    code: "CN",
+    name: "Chine",
+    flag: "🇨🇳"
+},
+
+{
+    code: "CY",
+    name: "Chypre",
+    flag: "🇨🇾"
+},
+
+{
+    code: "CO",
+    name: "Colombie",
+    flag: "🇨🇴"
+},
+
+{
+    code: "KM",
+    name: "Comores",
+    flag: "🇰🇲"
+},
+
+{
+    code: "CG",
+    name: "Congo",
+    flag: "🇨🇬"
+},
+
+{
+    code: "KP",
+    name: "Corée du Nord",
+    flag: "🇰🇵"
+},
+
+{
+    code: "KR",
+    name: "Corée du Sud",
+    flag: "🇰🇷"
+},
+
+{
+    code: "CR",
+    name: "Costa Rica",
+    flag: "🇨🇷"
+},
+
+{
+    code: "CI",
+    name: "Côte d'Ivoire",
+    flag: "🇨🇮"
+},
+
+{
+    code: "HR",
+    name: "Croatie",
+    flag: "🇭🇷"
+},
+
+{
+    code: "CU",
+    name: "Cuba",
+    flag: "🇨🇺"
+},
+
+{
+    code: "DK",
+    name: "Danemark",
+    flag: "🇩🇰"
+},
+
+{
+    code: "DJ",
+    name: "Djibouti",
+    flag: "🇩🇯"
+},
+
+{
+    code: "EG",
+    name: "Égypte",
+    flag: "🇪🇬"
+},
+
+{
+    code: "AE",
+    name: "Émirats arabes unis",
+    flag: "🇦🇪"
+},
+
+{
+    code: "EC",
+    name: "Équateur",
+    flag: "🇪🇨"
+},
+
+{
+    code: "ES",
+    name: "Espagne",
+    flag: "🇪🇸"
+},
+
+{
+    code: "EE",
+    name: "Estonie",
+    flag: "🇪🇪"
+},
+
+{
+    code: "US",
+    name: "États-Unis",
+    flag: "🇺🇸"
+},
+
+{
+    code: "ET",
+    name: "Éthiopie",
+    flag: "🇪🇹"
+},
+
+{
+    code: "FI",
+    name: "Finlande",
+    flag: "🇫🇮"
+},
+
+{
+    code: "FR",
+    name: "France",
+    flag: "🇫🇷"
+},
+
+{
+    code: "GA",
+    name: "Gabon",
+    flag: "🇬🇦"
+},
+
+{
+    code: "GM",
+    name: "Gambie",
+    flag: "🇬🇲"
+},
+
+{
+    code: "GH",
+    name: "Ghana",
+    flag: "🇬🇭"
+},
+
+{
+    code: "GR",
+    name: "Grèce",
+    flag: "🇬🇷"
+},
+
+{
+    code: "GT",
+    name: "Guatemala",
+    flag: "🇬🇹"
+},
+
+{
+    code: "GN",
+    name: "Guinée",
+    flag: "🇬🇳"
+},
+
+{
+    code: "HT",
+    name: "Haïti",
+    flag: "🇭🇹"
+},
+
+{
+    code: "HN",
+    name: "Honduras",
+    flag: "🇭🇳"
+},
+
+{
+    code: "HU",
+    name: "Hongrie",
+    flag: "🇭🇺"
+},
+
+{
+    code: "IN",
+    name: "Inde",
+    flag: "🇮🇳"
+},
+
+{
+    code: "ID",
+    name: "Indonésie",
+    flag: "🇮🇩"
+},
+
+{
+    code: "IQ",
+    name: "Irak",
+    flag: "🇮🇶"
+},
+
+{
+    code: "IR",
+    name: "Iran",
+    flag: "🇮🇷"
+},
+
+{
+    code: "IE",
+    name: "Irlande",
+    flag: "🇮🇪"
+},
+
+{
+    code: "IS",
+    name: "Islande",
+    flag: "🇮🇸"
+},
+
+{
+    code: "IT",
+    name: "Italie",
+    flag: "🇮🇹"
+},
+
+{
+    code: "JM",
+    name: "Jamaïque",
+    flag: "🇯🇲"
+},
+
+{
+    code: "JP",
+    name: "Japon",
+    flag: "🇯🇵"
+},
+
+{
+    code: "KE",
+    name: "Kenya",
+    flag: "🇰🇪"
+},
+
+{
+    code: "LA",
+    name: "Laos",
+    flag: "🇱🇦"
+},
+
+{
+    code: "LB",
+    name: "Liban",
+    flag: "🇱🇧"
+},
+
+{
+    code: "LR",
+    name: "Libéria",
+    flag: "🇱🇷"
+},
+
+{
+    code: "LY",
+    name: "Libye",
+    flag: "🇱🇾"
+},
+
+{
+    code: "LU",
+    name: "Luxembourg",
+    flag: "🇱🇺"
+},
+
+{
+    code: "MG",
+    name: "Madagascar",
+    flag: "🇲🇬"
+},
+
+{
+    code: "MY",
+    name: "Malaisie",
+    flag: "🇲🇾"
+},
+
+{
+    code: "ML",
+    name: "Mali",
+    flag: "🇲🇱"
+},
+
+{
+    code: "MA",
+    name: "Maroc",
+    flag: "🇲🇦"
+},
+
+{
+    code: "MU",
+    name: "Maurice",
+    flag: "🇲🇺"
+},
+
+{
+    code: "MX",
+    name: "Mexique",
+    flag: "🇲🇽"
+},
+
+{
+    code: "MC",
+    name: "Monaco",
+    flag: "🇲🇨"
+},
+
+{
+    code: "MZ",
+    name: "Mozambique",
+    flag: "🇲🇿"
+},
+
+{
+    code: "NA",
+    name: "Namibie",
+    flag: "🇳🇦"
+},
+
+{
+    code: "NP",
+    name: "Népal",
+    flag: "🇳🇵"
+},
+
+{
+    code: "NI",
+    name: "Nicaragua",
+    flag: "🇳🇮"
+},
+
+{
+    code: "NE",
+    name: "Niger",
+    flag: "🇳🇪"
+},
+
+{
+    code: "NG",
+    name: "Nigéria",
+    flag: "🇳🇬"
+},
+
+{
+    code: "NO",
+    name: "Norvège",
+    flag: "🇳🇴"
+},
+
+{
+    code: "NZ",
+    name: "Nouvelle-Zélande",
+    flag: "🇳🇿"
+},
+
+{
+    code: "UG",
+    name: "Ouganda",
+    flag: "🇺🇬"
+},
+
+{
+    code: "PK",
+    name: "Pakistan",
+    flag: "🇵🇰"
+},
+
+{
+    code: "PA",
+    name: "Panama",
+    flag: "🇵🇦"
+},
+
+{
+    code: "PY",
+    name: "Paraguay",
+    flag: "🇵🇾"
+},
+
+{
+    code: "NL",
+    name: "Pays-Bas",
+    flag: "🇳🇱"
+},
+
+{
+    code: "PE",
+    name: "Pérou",
+    flag: "🇵🇪"
+},
+
+{
+    code: "PH",
+    name: "Philippines",
+    flag: "🇵🇭"
+},
+
+{
+    code: "PL",
+    name: "Pologne",
+    flag: "🇵🇱"
+},
+
+{
+    code: "PT",
+    name: "Portugal",
+    flag: "🇵🇹"
+},
+
+{
+    code: "QA",
+    name: "Qatar",
+    flag: "🇶🇦"
+},
+
+{
+    code: "DO",
+    name: "République dominicaine",
+    flag: "🇩🇴"
+},
+
+{
+    code: "RO",
+    name: "Roumanie",
+    flag: "🇷🇴"
+},
+
+{
+    code: "GB",
+    name: "Royaume-Uni",
+    flag: "🇬🇧"
+},
+
+{
+    code: "RU",
+    name: "Russie",
+    flag: "🇷🇺"
+},
+
+{
+    code: "RW",
+    name: "Rwanda",
+    flag: "🇷🇼"
+},
+
+{
+    code: "SN",
+    name: "Sénégal",
+    flag: "🇸🇳"
+},
+
+{
+    code: "RS",
+    name: "Serbie",
+    flag: "🇷🇸"
+},
+
+{
+    code: "SG",
+    name: "Singapour",
+    flag: "🇸🇬"
+},
+
+{
+    code: "SO",
+    name: "Somalie",
+    flag: "🇸🇴"
+},
+
+{
+    code: "SD",
+    name: "Soudan",
+    flag: "🇸🇩"
+},
+
+{
+    code: "LK",
+    name: "Sri Lanka",
+    flag: "🇱🇰"
+},
+
+{
+    code: "SE",
+    name: "Suède",
+    flag: "🇸🇪"
+},
+
+{
+    code: "CH",
+    name: "Suisse",
+    flag: "🇨🇭"
+},
+
+{
+    code: "SY",
+    name: "Syrie",
+    flag: "🇸🇾"
+},
+
+{
+    code: "TZ",
+    name: "Tanzanie",
+    flag: "🇹🇿"
+},
+
+{
+    code: "TD",
+    name: "Tchad",
+    flag: "🇹🇩"
+},
+
+{
+    code: "TH",
+    name: "Thaïlande",
+    flag: "🇹🇭"
+},
+
+{
+    code: "TG",
+    name: "Togo",
+    flag: "🇹🇬"
+},
+
+{
+    code: "TN",
+    name: "Tunisie",
+    flag: "🇹🇳"
+},
+
+{
+    code: "TR",
+    name: "Turquie",
+    flag: "🇹🇷"
+},
+
+{
+    code: "UA",
+    name: "Ukraine",
+    flag: "🇺🇦"
+},
+
+{
+    code: "UY",
+    name: "Uruguay",
+    flag: "🇺🇾"
+},
+
+{
+    code: "VE",
+    name: "Venezuela",
+    flag: "🇻🇪"
+},
+
+{
+    code: "VN",
+    name: "Viêt Nam",
+    flag: "🇻🇳"
+},
+
+{
+    code: "YE",
+    name: "Yémen",
+    flag: "🇾🇪"
+},
+
+{
+    code: "ZM",
+    name: "Zambie",
+    flag: "🇿🇲"
+},
+
+{
+    code: "ZW",
+    name: "Zimbabwe",
+    flag: "🇿🇼"
+}
+
+];
+
+// ==========================================
+// End
+// ==========================================
+
+Object.freeze(COUNTRIES);
+
+console.log(
+    "NetView Countries Loaded:",
+    COUNTRIES.length
+);
+];
