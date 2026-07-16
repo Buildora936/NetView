74ca60d2c5eca4e91f7d0062f4541045ecc2ba53
BUILDORA <bjosephbrooks07@gmail.com>
2026-07-16 14:35:10 -0400

    Update signup.js

--- js/pages/signup.js
+++ js/pages/signup.js
@@ -1,1 +1,133 @@
+// ==========================================
+// Imports
+// ==========================================
 
+import { supabase } from "../core/supabase.js";
+
+
+// ==========================================
+// DOM
+// ==========================================
+
+const usernameInput =
+document.getElementById("username");
+
+const usernameMessage =
+document.getElementById("usernameMessage");
+
+
+// ==========================================
+// Username
+// ==========================================
+
+let usernameTimer = null;
+
+
+// Nettoyage
+
+function cleanUsername(value){
+
+    return value
+
+        .toLowerCase()
+
+        .replace(/\s+/g,"")
+
+        .replace(/[^a-z0-9._]/g,"");
+
+}
+
+
+// Vérification
+
+async function checkUsername(){
+
+    const username =
+    usernameInput.value;
+
+    if(username.length < 3){
+
+        usernameMessage.textContent =
+        "Minimum 3 caractères.";
+
+        usernameMessage.className =
+        "nv-help username-invalid";
+
+        return;
+
+    }
+
+    usernameMessage.textContent =
+    "Vérification...";
+
+    usernameMessage.className =
+    "nv-help username-check";
+
+    const {
+
+        data,
+
+        error
+
+    } = await supabase
+
+        .from("profiles")
+
+        .select("username")
+
+        .eq("username",username)
+
+        .maybeSingle();
+
+    if(error){
+
+        usernameMessage.textContent =
+        "Impossible de vérifier.";
+
+        usernameMessage.className =
+        "nv-help username-invalid";
+
+        return;
+
+    }
+
+    if(data){
+
+        usernameMessage.textContent =
+        "Nom déjà utilisé.";
+
+        usernameMessage.className =
+        "nv-help username-invalid";
+
+        return;
+
+    }
+
+    usernameMessage.textContent =
+    "Nom disponible.";
+
+    usernameMessage.className =
+    "nv-help username-valid";
+
+}
+
+
+// Event
+
+usernameInput.addEventListener("input",()=>{
+
+    usernameInput.value =
+    cleanUsername(usernameInput.value);
+
+    clearTimeout(usernameTimer);
+
+    usernameTimer =
+    setTimeout(
+
+        checkUsername,
+
+        500
+
+    );
+
+});
