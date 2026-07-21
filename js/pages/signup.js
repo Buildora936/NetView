37e5787453dcef3309ec7f46c0f84bdcc06711a1
BUILDORA <bjosephbrooks07@gmail.com>
2026-07-21 14:32:25 -0400

    Remove unused import of COUNTRIES
    Remove unused COUNTRIES import from signup.js

--- js/pages/signup.js
+++ js/pages/signup.js
@@ -4,8 +4,7 @@
 
 import { supabase } from "../core/supabase.js";
 
-import { COUNTRIES }
-from "../assets/countries.js";
+
 // ==========================================
 // Already Logged
 // ==========================================
