02936bbc560df7ad64fd80f8d77566ebecc5f56c
BUILDORA <bjosephbrooks07@gmail.com>
2026-08-16 12:29:48 -0400

    Update index.js

--- js/pages/index.js
+++ js/pages/index.js
@@ -2109,7 +2109,15 @@ function closeContextMenu(){
 
 }
 
+function escapeHtml(value) {
 
+    return String(value ?? "")
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
 
 
 
